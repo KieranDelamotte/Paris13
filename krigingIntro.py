1a07d30051f2bd054d1825116d35f47ed03be6e2
Kieran Delamotte <delamotte@imacs.polytechnique.fr>
2024-02-08 14:04:21 +0100

    wip

--- krigingIntro.py
+++ krigingIntro.py
@@ -5,6 +5,7 @@ import sklearn
 
 X = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1)
 xt = np.array([0, np.pi / 2, np.pi, 3 * np.pi / 2, 2 * np.pi]).reshape(-1, 1)
+xt = np.array([0.2, 1.1, 2.7, 4.5, 5.7]).reshape(-1, 1)
 yt = np.sin(xt).reshape(-1, 1)
 print(np.mean(yt))
 from sklearn.gaussian_process import GaussianProcessRegressor
@@ -16,7 +17,11 @@ from sklearn.gaussian_process.kernels import (
     RBF,
 )
 
-gpr = GaussianProcessRegressor(normalize_y=True).fit(xt, yt)
+kernel = ConstantKernel() * RBF()
+#kernel = RBF()
+
+gpr = GaussianProcessRegressor(kernel, normalize_y=True,n_restarts_optimizer=5,).fit(xt, yt)
+#gpr = GaussianProcessRegressor(normalize_y=True).fit(xt, yt)
 y_mean, y_std = gpr.predict(X, return_std=True)
 ys = gpr.sample_y(X, 5)
 
@@ -38,12 +43,13 @@ ax.plot(X[:, 0], ys[:, 0, 0], "--k", label="one conditioned realization")
 ax.grid()
 
 
-kernel = ConstantKernel(1.0, constant_value_bounds="fixed") * RBF(
-    1.0, length_scale_bounds="fixed"
-)
+#kernel = ConstantKernel(1.0, constant_value_bounds="fixed") * RBF(
+#    1.0, length_scale_bounds="fixed"
+#)
 
 print(gpr.get_params())
-print(kernel)
+print(gpr.kernel_)
+print(gpr.kernel_.theta)
 
 l = 1.0
 sigma = 1.0
@@ -59,5 +65,45 @@ ax.legend()
 ax.set_xlabel(r"$t$")
 ax.set_ylabel(r"$Z_t$")
 ax.set_title(r"Simple Gaussian Process Regression exemple: $x \mapsto \sin(x)$")
+
+
+
+
+def build_covariance_matrix(xt, params):
+    sigma, ll = params
+    return sigma**2 *  np.exp(-np.abs(np.subtract.outer(xt.ravel(), xt.ravel())) ** 2 / (2.0 * ll ** 2))
+
+
+def log_likelihood(y, K):
+    """log likelihood function"""
+    n = 5
+    ll = -0.5 * np.dot(y.T, np.linalg.solve(K, y)) - 0.5 * np.log(np.abs(np.linalg.det(K))) - n / 2 * np.log(2*np.pi)
+    return ll
+
+
+ll = log_likelihood(yt, Ktt)
+print(ll)
+print(np.info(Ktt))
+
+
+def foo(params):
+    Ktt = build_covariance_matrix(xt, params)
+    return -log_likelihood(yt, Ktt)
+    
+from scipy.optimize import minimize
+
+bnds = ((0, None), (0, None))
+x0 = np.array([1.0, 1.0])
+print(foo(x0))
+res = minimize(foo, x0, bounds=bnds,method='L-BFGS-B')
+print(res)
+
+Ktt = build_covariance_matrix(xt, res.x)
+z = np.linalg.solve(Ktt, (yt - np.mean(yt)))
+Knn = res.x[0]**2 * np.exp(-np.abs(np.subtract.outer(xt.ravel(), X.ravel())) ** 2 / (2.0 * (res.x[1]) ** 2))
+esp2 = np.dot(Knn.T, z)
+ax.plot(X[:, 0], esp2 + np.mean(yt), "--", color="saddlebrown", label=r"$\mathbb{E}[Y]$")
+
+
 
 plt.show()
